c9be0a878b34a7fe4b013c9331c643183dedd5d1
DuongTuanKhang <khangnha0811@gmail.com>
2026-03-10 14:27:19 +0700

    CRUD products API

--- routes/products.js
+++ routes/products.js
@@ -1,14 +1,26 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-let productModel = require('../schemas/products')
+let productModel = require("../schemas/products");
 
-/* GET users listing. */
-router.get('/', async function(req, res, next) {
-  let result = await productModel.find({}) 
+// GET all products
+router.get("/", async function (req, res, next) {
+  let result = await productModel.find({});
   res.send(result);
 });
-router.get('/id', function(req, res, next) {
-  res.send('hahah');
+
+// POST create a new product
+router.post("/", async function (req, res, next) {
+  try {
+    let newProduct = new productModel(req.body);
+    let result = await newProduct.save();
+    res.status(201).send(result);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get("/id", function (req, res, next) {
+  res.send("hahah");
 });
 
 module.exports = router;
